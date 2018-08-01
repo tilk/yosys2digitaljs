289fe18e27c3686c437adad7983d10eff04cf0a8
Marek Materzok <tilk@tilk.eu>
2018-08-01 23:50:04 +0200

    Changed to export to DigitalJS.

--- process.js
+++ process.js
@@ -10,11 +10,7 @@ const header = `<!doctype html>
 <html>
   <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
-    <script type="text/javascript" src="simcir.js"></script>
-    <link rel="stylesheet" type="text/css" href="simcir.css" />
-    <script type="text/javascript" src="simcir-basicset.js"></script>
-    <link rel="stylesheet" type="text/css" href="simcir-basicset.css" />
-    <script type="text/javascript" src="simcir-library.js"></script>
+    <script type="text/javascript" src="main.js"></script>
     <title></title>
   </head>
   <body>`;
@@ -34,8 +30,8 @@ function module_deps(data) {
 }
 
 function order_ports(data) {
-    const unmap = {A: 'in0', Y: 'out0'};
-    const binmap = {A: 'in0', B: 'in1', Y: 'out0'};
+    const unmap = {A: 'in', Y: 'out'};
+    const binmap = {A: 'in1', B: 'in2', Y: 'out'};
     const out = {};
     ['$and', '$or', '$xor', '$xnor'].forEach((nm) => out[nm] = binmap);
     ['$not'].forEach((nm) => out[nm] = unmap);
@@ -44,34 +40,35 @@ function order_ports(data) {
         const portmap = {};
         const ins = [], outs = [];
         for (const pname in mod.ports) {
+            portmap[pname] = pname;/*
             const port = mod.ports[pname];
             const pdata = {name: pname, num: port.bits[0]};
             switch (port.direction) {
                 case 'input': ins.push(pdata); break;
                 case 'output': outs.push(pdata); break;
                 default: throw Error("Invalid port direction: " + port.direction);
-            }
-        }
+            }*/
+        }/*
         function comp(a, b) {
             return a.num - b.num;
         }
         ins.sort(comp);
         outs.sort(comp);
         for (const k in ins) portmap[ins[k].name] = "in" + k;
-        for (const k in outs) portmap[outs[k].name] = "out" + k;
+        for (const k in outs) portmap[outs[k].name] = "out" + k;*/
         out[name] = portmap;
     }
     return out;
 }
 
 function yosys_to_simcir(data, portmaps) {
-    const typemap = {
+    const typemap = {}; /*{
         '$and': 'AND',
         '$or': 'OR',
         '$xor': 'XOR',
         '$xnor': 'XNOR',
         '$not': 'NOT'
-    };
+    };*/
     const out = {};
     for (const name in data.modules) {
         let n = 0;
@@ -87,17 +84,17 @@ function yosys_to_simcir(data, portmaps) {
         function add_net_source(k, d, p) {
             let net = get_net(k);
             assert(net.source === undefined);
-            net.source = d+'.'+p;
+            net.source = { id: d, port: p };
         }
         function add_net_target(k, d, p) {
             let net = get_net(k);
-            net.targets.push(d+'.'+p);
+            net.targets.push({ id: d, port: p });
         }
         const mod = data.modules[name];
         const mout = {
-            width: 800,
-            height: 500,
-            devices: [],
+//            width: 800,
+//            height: 500,
+            devices: {},
             connectors: []
         }
         out[name] = mout;
@@ -105,34 +102,35 @@ function yosys_to_simcir(data, portmaps) {
             const port = mod.ports[pname];
             const dname = gen_name();
             let dev = {
-                id: dname,
-                x: 0,
-                y: 0,
+//                id: dname,
+//                x: 0,
+//                y: 0,
                 label: pname,
+                net: pname,
                 order: n
             };
             assert(port.bits.length == 1);
             switch (port.direction) {
                 case 'input':
-                    dev.type = 'In';
-                    add_net_source(port.bits[0], dname, 'out0');
+                    dev.type = '$input';
+                    add_net_source(port.bits[0], dname, 'out');
                     break;
                 case 'output':
-                    dev.type = 'Out';
-                    add_net_target(port.bits[0], dname, 'in0');
+                    dev.type = '$output';
+                    add_net_target(port.bits[0], dname, 'in');
                     break;
                 default: throw Error('Invalid port direction: ' + port.direction);
             }
-            mout.devices.push(dev);
+            mout.devices[dname] = dev;
         }
         for (const cname in mod.cells) {
             const cell = mod.cells[cname];
             const portmap = portmaps[cell.type];
             const dname = gen_name();
             let dev = {
-                id: dname,
-                x: 0,
-                y: 0,
+//                id: dname,
+//                x: 0,
+//                y: 0,
                 label: cname
             };
             if (cell.type in typemap)
@@ -171,12 +169,12 @@ function yosys_to_simcir(data, portmaps) {
                         throw Error('Invalid port direction: ' + pdir);
                 }
             }
-            mout.devices.push(dev);
+            mout.devices[dname] = dev;
         }
         for (const nnum in nets) {
             const net = nets[nnum];
             for (const target in net.targets)
-                mout.connectors.push({from: net.targets[target], to: net.source});
+                mout.connectors.push({to: net.targets[target], from: net.source});
         }
     }
     return out
@@ -200,7 +198,7 @@ function layout_circuit(circ) {
     }
 
     for (const conn of circ.connectors) {
-        g.setEdge(conn.from.split('.')[0], conn.to.split('.')[0]);
+        g.setEdge(conn.from.id, conn.to.id);
     }
 
     dagre.layout(g);
@@ -227,20 +225,20 @@ function layout_circuits(circs) {
 let obj = JSON.parse(fs.readFileSync('output.json', 'utf8'));
 let portmaps = order_ports(obj);
 let out = yosys_to_simcir(obj, portmaps);
-layout_circuits(out);
+//layout_circuits(out);
 let toporder = topsort(module_deps(obj));
 toporder.pop();
 let toplevel = toporder.pop();
-console.log(header);
-console.log('<script>');
-for (const x of toporder) {
-    console.log('simcir.registerDevice("'+x+'", ');
-    console.log(JSON.stringify(out[x], null, 2));
-    console.log(');');
+let output = out[toplevel];
+for (const name in output.devices) {
+    const dev = output.devices[name];
+    if (dev.type == '$input') dev.type = '$button';
+    if (dev.type == '$output') dev.type = '$not';
 }
-console.log('</script>');
-console.log('<div class="simcir">');
+output.subcircuits = {};
+for (const x of toporder) output.subcircuits[x] = out[x];
+console.log(header);
+console.log('<div id="paper"></div><script>const circuit = new digitaljs.Circuit(');
 console.log(JSON.stringify(out[toplevel], null, 2));
-console.log('</div>');
-console.log('</body></html>');
+console.log(');const paper = circuit.displayOn($(\'#paper\'));</script></body></html>');
 
